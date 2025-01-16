7235bdf0684f23d1347a96ebd342bad4c57f9047
Sujal Sharma <62907239+ersujalsharma@users.noreply.github.com>
2025-01-16 10:09:58 +0530

    Generic Revision

--- STG/src/Main.java
+++ STG/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(sum(5.0,6.0));
+    }
+    // create generic sum method here
+    public static <T extends Number> Number sum(T a, T b) {
+        return a.doubleValue()+b.doubleValue();
     }
 }
